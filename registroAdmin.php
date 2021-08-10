f972d246d62a13017eeb2b52c221c6452a4cf0b5
Jos3Lu1s <5joselbautistac5@gmail.com>
2021-08-09 19:19:15 -0500

    Comparar password

--- registroAdmin.php
+++ registroAdmin.php
@@ -1,66 +1,30 @@
 <?php
 require_once('./sesiones/database.php');
 
-### Validaciones
-
-if(isset($_POST['name_form'])){
+if(!empty($_POST)){
     $name = $_POST['name_form'];
     $lastname = $_POST['lastname_form'];
     $email = $_POST['email_form'];
     $password_usr = $_POST['password_form'];
-    
-    $campos = array();
-    
-    if(strlen($password_usr) < 8){
-        array_push($campos, "El Password debe ser mayor a 8 caracteres");
-    }
-
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        array_push($campos, "Ingrese un correo valido");
-    }
-
-    if($name == "" || $lastname == "" || $email == "" || $password_usr == ""){
-        array_push($campos, "No puede haber campos vacios");
-    }
-
-    if(strlen($name) > 15){
-        array_push($campos, "Nombre demasiado grande");
-    }
-
-    if(strlen($lastname) > 15){
-        array_push($campos, "Apellido demasiado grande");
-    }
-
-    if(count($campos) > 0){
-        for($i = 0; $i < count($campos); $i++){
-            $_SESSION['message']=$campos[$i];
-        }
+    $password_confirm = $_POST['cpassword_form'];
+    $idcat = 1;
+    if($password_usr == $password_confirm){
+        
+        $sql = "INSERT INTO users (name_user, las_name, email_user, password_user, ID_cat) VALUES (:name_user, :las_name, :email_user, :password_user, :ID_cat)";
+        $query = $pdo->prepare($sql);
+        $query->execute([
+            'name_user'=>$name,
+            'las_name'=>$lastname,
+            'email_user'=>$email,
+            'password_user'=>$password_usr,
+            'ID_cat'=>$idcat
+        ]);
+        header("Location: administrador.php");
     }
     else{
-        
-        $_SESSION['message2']='Datos Correctos';
-    
-        ###Insert
-
-        if(!empty($_POST)){
-            $name = $_POST['name_form'];
-            $lastname = $_POST['lastname_form'];
-            $email = $_POST['email_form'];
-            $password_usr = $_POST['password_form'];
-            $idcat = 1;
-            
-            $sql = "INSERT INTO users (name_user, las_name, email_user, password_user, ID_cat) VALUES (:name_user, :las_name, :email_user, :password_user, :ID_cat)";
-            $query = $pdo->prepare($sql);
-            $query->execute([
-                'name_user'=>$name,
-                'las_name'=>$lastname,
-                'email_user'=>$email,
-                'password_user'=>$password_usr,
-                'ID_cat'=>$idcat
-            ]);
-
-            header("Location: administrador.php");
-        }
+        header("Location: administrador.php");
     }
+    
 }
+
 ?>
--- sesiones/signup y validacion.php
+++ sesiones/signup y validacion.php
@@ -8,6 +8,7 @@ if(isset($_POST['name_form'])){
     $lastname = $_POST['lastname_form'];
     $email = $_POST['email_form'];
     $password_usr = $_POST['password_form'];
+    $password_confirm = $_POST['cpassword_form'];
     
     $campos = array();
     
@@ -30,6 +31,10 @@ if(isset($_POST['name_form'])){
     if(strlen($lastname) > 15){
         array_push($campos, "Apellido demasiado grande");
     }
+    
+    if($password_usr!=$password_confirm){
+        array_push($campos, "las contraseñas no coinciden");
+    }
 
     if(count($campos) > 0){
         for($i = 0; $i < count($campos); $i++){
@@ -47,17 +52,23 @@ if(isset($_POST['name_form'])){
             $lastname = $_POST['lastname_form'];
             $email = $_POST['email_form'];
             $password_usr = $_POST['password_form'];
+            $password_confirm = $_POST['cpassword_form'];
             $idcat = 1;
+
+            if($password_usr == $password_confirm){
+                
+                $sql = "INSERT INTO users (name_user, las_name, email_user, password_user, ID_cat) VALUES (:name_user, :las_name, :email_user, :password_user, :ID_cat)";
+                $query = $pdo->prepare($sql);
+                $query->execute([
+                    'name_user'=>$name,
+                    'las_name'=>$lastname,
+                    'email_user'=>$email,
+                    'password_user'=>$password_usr,
+                    'ID_cat'=>$idcat
+                ]);
+
+            }
             
-            $sql = "INSERT INTO users (name_user, las_name, email_user, password_user, ID_cat) VALUES (:name_user, :las_name, :email_user, :password_user, :ID_cat)";
-            $query = $pdo->prepare($sql);
-            $query->execute([
-                'name_user'=>$name,
-                'las_name'=>$lastname,
-                'email_user'=>$email,
-                'password_user'=>$password_usr,
-                'ID_cat'=>$idcat
-            ]);
         }
     }
 }
